4327ec54c8b514055f7d269301e1ce4b3878e18a
iRishabhSingh <rishab18011@gmail.com>
2024-08-10 23:52:18 +0530

    Initial commit of InvoceAI

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
+      <div>InvoceAI</div>
+    </main>
+  );
+}
